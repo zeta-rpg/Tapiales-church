50777074e5c6e9b227e782f9d6421df493d92b22
Samuel Campos <zckfr000@gmail.com>
2025-08-19 18:54:44 -0300

    config GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://zeta-rpg.github.io/Tapiales-church",
+  base: "/Tapiales-church/",
+});
