34bdd4c3319d0cdefa870616ec5b5ab6e2c407d3
zeta-rpg <abel@Tapiales.localdomain>
2025-09-02 01:19:10 -0300

    actualizaciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/static";
 
 export default defineConfig({
   site: "https://zeta-rpg.github.io/Tapiales-church",
-  base: "/Tapiales-church/",
+  base: "/",
+  adapter: netlify(),
 });
+
